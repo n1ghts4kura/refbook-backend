1da4504d77658d1f6a0359fb113db9c1e80d135d
n1ghts4kura <1825259836@qq.com>
2025-08-02 17:37:54 +0800

    change the way of POST from form to application/json

--- module/routes/user/create.py
+++ module/routes/user/create.py
@@ -2,15 +2,21 @@
 # 创建新用户
 #
 
-from fastapi import Form
+from pydantic import BaseModel
 
 from ...database import user as user_db
 from .router import router
 from ...utils.security import get_password_hash
 from ...utils import log
 
+
+class UserCreateRequest(BaseModel):
+    username: str
+    password: str
+
+
 @router.post("/create")
-async def create_user(username: str = Form(...), password: str = Form(...)):
+async def create_user(user: UserCreateRequest):
     """
     创建新用户
     
@@ -23,7 +29,7 @@ async def create_user(username: str = Form(...), password: str = Form(...)):
     """
 
     log.info("first check if the user already exists")
-    existing_user = await user_db.get_user_by_username(username)
+    existing_user = await user_db.get_user_by_username(user.username)
     if not isinstance(existing_user, dict):
         log.error(f"User [{existing_user.username}] already exists.")
         return {
@@ -32,7 +38,7 @@ async def create_user(username: str = Form(...), password: str = Form(...)):
         }
 
     log.info("the user doesn't exist, so now create a new one")
-    new_user = await user_db.new_user(username, get_password_hash(password))
+    new_user = await user_db.new_user(user.username, get_password_hash(user.password))
     if isinstance(new_user, dict):
         reason = new_user.get("message", "Failed to create user")
         log.warning(f"-> {reason}")
@@ -42,7 +48,7 @@ async def create_user(username: str = Form(...), password: str = Form(...)):
         }
     
     # successfully created the user
-    log.info(f"successfully created user [{username}]")
+    log.info(f"successfully created user [{user.username}]")
     return {
         "type": "success"
     }
